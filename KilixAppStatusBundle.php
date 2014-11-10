2ae292de7cebe3f0ff0b65e8c4f712cc46ef7592
Alexandre BODIN <abodin@kilix.fr>
2014-11-10 21:32:43 +0100

    wip

--- KilixAppStatusBundle.php
+++ KilixAppStatusBundle.php
@@ -3,7 +3,16 @@
 namespace Kilix\AppStatusBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Kilix\AppStatusBundle\DependencyInjection\Compiler\AppStatusCompilerPass;
 
 class KilixAppStatusBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AppStatusCompilerPass());
+    }
 }
